6eb061f8723c455c0c0eb045a7a06d9d1a0a114a
risqi2 <risqiikhsani12@gmail.com>
2022-10-08 09:00:32 +0700

    test

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+
+import counterReducer from './slices/counterSlice'
+
+
+export default configureStore({
+  reducer: {
+    counter:counterReducer,
+  },
+})
