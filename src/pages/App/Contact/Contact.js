5dff504bd6a5b5302487a3649f9a82157f3f2fa1
risqi2 <risqiikhsani12@gmail.com>
2023-02-04 01:06:32 +0700

    test

--- src/pages/App/Contact/Contact.js
+++ src/pages/App/Contact/Contact.js
@@ -1,14 +1,14 @@
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import { styled, Box, Stack } from "@mui/system";
-import { Button, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
+import { Button, Chip, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
 import Tab from "@mui/material/Tab";
-import ContactTable from "./ContactTable";
-import ContactSearchSortBy from "./ContactSearchSortBy";
+import Tabs from "@mui/material/Tabs";
+import { Box, Stack } from "@mui/system";
+import PropTypes from "prop-types";
+import * as React from "react";
 import ContactFilter from "./ContactFilter";
+import ContactSearchSortBy from "./ContactSearchSortBy";
+import ContactTable from "./ContactTable";
 
 // const Item = styled(Paper)(({ theme }) => ({
 //   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -49,6 +49,29 @@ function a11yProps(index) {
   };
 }
 
+const tableColumn = [
+  {
+    index: 0,
+    name: "Connected",
+    count: 20,
+  },
+  {
+    index: 1,
+    name: "Friend Requests",
+    count: 10,
+  },
+  {
+    index: 2,
+    name: "Waiting Requests",
+    count: 5,
+  },
+  {
+    index: 3,
+    name: "Blocked users",
+    count: 2,
+  },
+]
+
 export default function Contact(props) {
   const [value, setValue] = React.useState(0);
 
@@ -81,32 +104,56 @@ export default function Contact(props) {
           }}
         >
           <Paper>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-            >
-              <Tab label="Connected" {...a11yProps(0)} />
-              <Tab label="Request in Pending" {...a11yProps(1)} />
-              <Tab label="Request out Pending" {...a11yProps(2)} />
-              <Tab label="Blocked" {...a11yProps(3)} />
-            </Tabs>
-          </Box>
-          <TabPanel value={value} index={0}>
-            <ContactSearchSortBy/>
-            <ContactFilter/>
-            <ContactTable/>
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            Item Three
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            Item Two
-          </TabPanel>
-          <TabPanel value={value} index={3}>
-            Item Three
-          </TabPanel>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+              >
+                {
+                  tableColumn.map((a) => (
+                    <Tab key={a.index} sx={{ textTransform: 'none' }} label={
+                      <Stack
+                        direction="row"
+                        alignItems="center"
+                        spacing={2}
+                      >
+                        {a.name}
+                        <Chip size="small" variant="outlined" label={a.count} color="success" sx={{ ml: '5px' }} />
+                      </Stack>
+                    } {...a11yProps(a.index)} />
+                  ))
+                }
+                {/* <Tab sx={{textTransform:'none'}} label="Connected" {...a11yProps(0)} />
+              <Tab sx={{textTransform:'none'}} label={
+                <Stack
+                direction="row"
+                alignItems="center"
+                spacing={2}
+              >
+                Friend Request
+                <Chip size="small" variant="outlined" label="12" color="success" sx={{ml:'5px'}}/>
+              </Stack>
+                
+              } {...a11yProps(1)} />
+              <Tab sx={{textTransform:'none'}} label="Waiting Requests" {...a11yProps(2)} />
+              <Tab sx={{textTransform:'none'}} label="Blocked users" {...a11yProps(3)} /> */}
+              </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+              {/* <ContactSearchSortBy/>
+            <ContactFilter/> */}
+              <ContactTable />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Item Three
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              Item Two
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              Item Three
+            </TabPanel>
           </Paper>
 
         </Box>
--- src/pages/App/Contact/ContactTable.js
+++ src/pages/App/Contact/ContactTable.js
@@ -22,14 +22,16 @@ const rows = [
 export default function ContactTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Nickname</TableCell>
+            <TableCell align="right">Following</TableCell>
+            <TableCell align="right">Pinned</TableCell>
+            <TableCell align="right">Notify</TableCell>
+            <TableCell align="right">Connected Date</TableCell>
+            <TableCell align="right">Contact Groups</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
