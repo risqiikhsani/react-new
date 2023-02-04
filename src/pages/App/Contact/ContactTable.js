1d30be271ecfcb14404fd29f0de7b2b893702425
risqi2 <risqiikhsani12@gmail.com>
2023-02-04 18:33:51 +0700

    test

--- src/pages/App/Contact/ContactTable.js
+++ src/pages/App/Contact/ContactTable.js
@@ -8,17 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { memo } from 'react';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 function ContactTable(props) {
   return (
@@ -36,20 +26,24 @@ function ContactTable(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
+          {
+            props.data && props.data.map((a) => (
+              <TableRow
+              key={a.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {a.user.profile.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{a.relationship.nickname}</TableCell>
+              <TableCell align="right">{a.relationship.follow ? "true":"false"}</TableCell>
+              <TableCell align="right">{a.relationship.pin ? "true":"false"}</TableCell>
+              <TableCell align="right">{a.relationship.notification ? "true":"false"}</TableCell>
+              <TableCell align="right">-</TableCell>
+              <TableCell align="right">-</TableCell>
             </TableRow>
-          ))}
+            ))
+          }
         </TableBody>
       </Table>
     </TableContainer>
